12bce417099b56b15930a9f04bdda1ef55560d09
akshay <akshay.jain2@exlservice.com>
2019-07-10 10:44:20 +0000

    coded check brackets passed cases in assignment pdf

--- week1_basic_data_structures/1_brackets_in_code/check_brackets.py
+++ week1_basic_data_structures/1_brackets_in_code/check_brackets.py
@@ -14,17 +14,26 @@ def find_mismatch(text):
     for i, next in enumerate(text):
         if next in "([{":
             # Process opening bracket, write your code here
-            pass
+            opening_brackets_stack.append(Bracket(next,i))
 
         if next in ")]}":
             # Process closing bracket, write your code here
-            pass
+            if not are_matching( getattr(opening_brackets_stack.pop(), "char"), next ):
+                return i
+    if len(opening_brackets_stack) > 0:
+        return getattr( opening_brackets_stack.pop(), "position" )
+    return None
+
 
 
 def main():
     text = input()
     mismatch = find_mismatch(text)
     # Printing answer, write your code here
+    if mismatch is None:
+        print("Success")
+    else :
+        print(mismatch+1)
 
 
 if __name__ == "__main__":
